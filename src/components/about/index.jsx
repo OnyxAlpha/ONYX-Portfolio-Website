4dc908becb9f289c754dbacac8ef6bc4df622305
Samantha Adda <sammiedaga@gmail.com>
2024-03-23 22:46:52 +0000

    creating about us section

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,7 +1,19 @@
 export default function AboutUs() {
     return (
         <>
-        <p>About us goes here</p>
+        <section className="bg-slate-200">
+            <div>Logo</div>
+            <div>
+                <h1>About Us</h1>
+                <p>Welcome to Onyx, a passionate team of web developers, dedicated to crafting exceptional digital experiences.</p>
+                <p>With a combination of a great team, we collaborate to bring visions to life through code and creativity.</p>
+                <p>At Onyx, we specialize in creating custom websites and web applications that exceed client expectations.</p>
+                <p>From the initial concept to the final launch, we prioritize communication, transparency and attention to detail, ensuring a seamless and enjoyable experience for everyone involved.</p>
+                <p>When you choose Onyx, you are not just hiring a team of web developers, you are partnering with a dedicated group of young professionals who are commited to your success, delivering projects on time and within budget.</p>
+                <p>Get in touch with us today to duscuss your project and see how Onyx can make it a reality.</p>
+            </div>
+
+        </section>
         </>
     );
 };
