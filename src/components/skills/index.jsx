9d63fa8b13f3253acc70181cb4d8cb97aaf074da
Samantha Adda <sammiedaga@gmail.com>
2024-03-24 12:40:57 +0000

    skills component

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -1,7 +1,80 @@
+import React from "react";
+
 export default function Skills() {
-    return (
-        <>
-        <p>Skills go here</p>
-        </>
-    );
-};
+  return (
+    <>
+      <section>
+        <div className="text-center">
+          <h3>Our Skills</h3>
+        </div>
+
+        <div className="grid grid-cols-3 p-10 md:grid-cols-5">
+          <div className="bg-sky-200 text-center">
+            <p>icon</p>
+            <p>HTML5</p>
+          </div>
+
+          <div className="text-center">
+            <p>icon</p>
+            <p>CSS</p>
+          </div>
+
+          <div className="bg-sky-200 text-center">
+            <p>icon</p>
+            <p>JavaScript</p>
+          </div>
+
+          <div className="text-center">
+            <p>icon</p>
+            <p>React JS</p>
+          </div>
+
+          <div className="bg-sky-200 text-center">
+            <p>icon</p>
+            <p>Nodejs</p>
+          </div>
+
+          <div className="text-center">
+            <p>icon</p>
+            <p>MongoDB</p>
+          </div>
+
+          <div className="bg-sky-200 text-center">
+            <p>icon</p>
+            <p>GitHub</p>
+          </div>
+
+          <div className="text-center">
+            <p>icon</p>
+            <p>Tailwind</p>
+          </div>
+
+          <div className="bg-sky-200 text-center">
+            <p>icon</p>
+            <p>Responsive Design</p>
+          </div>
+
+          <div className="text-center">
+            <p>icon</p>
+            <p>Figma</p>
+          </div>
+
+          <div className="bg-sky-200 text-center">
+            <p>icon</p>
+            <p>APIs</p>
+          </div>
+
+          <div className="text-center">
+            <p>icon</p>
+            <p>Command Line</p>
+          </div>
+
+          <div className="bg-sky-200 col-span-3 text-center md:col-span-1">
+            <p>icon</p>
+            <p>Deployment and Hosting</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
