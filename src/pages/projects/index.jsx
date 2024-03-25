6d74e1942c97c62aa7094827c293ea73b098db90
Hannan-Laaraba <ashraweelaaraba@gmail.com>
2024-03-25 11:01:47 +0000

    initial commit

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,7 +1,145 @@
+import React from "react";
+
+
+
 export default function Projects() {
-    return (
-        <>
-        <p>Projects go here</p>
-        </>
-    );
-};
+  return (
+    <>
+     <section className="bg-gray-200">
+     <div class=" text-center p-10">
+        <h1 class="text-3xl">Projects</h1>
+      </div>
+
+      <section
+        id="Projects"
+        class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
+      >
+        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+          <a href="#">
+            <img
+              src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Product"
+              class="h-80 w-72 object-cover rounded-t-xl"
+            />
+            <div class="px-4 py-3 w-72">
+              <span class="text-gray-400 mr-3 uppercase text-xs">
+                Web Design
+              </span>
+              <p class="text-lg font-bold text-black truncate block capitalize">
+                AgroMart
+              </p>
+              <div class="flex items-center">
+                <button>Preview</button>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <div class="ml-auto">
+                  
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+          <a href="#">
+            <img
+              src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Product"
+              class="h-80 w-72 object-cover rounded-t-xl"
+            />
+            <div class="px-4 py-3 w-72">
+              <span class="text-gray-400 mr-3 uppercase text-xs">Web App</span>
+              <p class="text-lg font-bold text-black truncate block capitalize">
+                OnyxCinema
+              </p>
+              <div class="flex items-center">
+                <div class="ml-auto">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+          <a href="#">
+            <img
+              src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Product"
+              class="h-80 w-72 object-cover rounded-t-xl"
+            />
+            <div class="px-4 py-3 w-72">
+              <span class="text-gray-400 mr-3 uppercase text-xs">Web App</span>
+              <p class="text-lg font-bold text-black truncate block capitalize">
+                OnyxCinema
+              </p>
+              <div class="flex items-center">
+                <div class="ml-auto">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+          <a href="#">
+            <img
+              src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Product"
+              class="h-80 w-72 object-cover rounded-t-xl"
+            />
+            <div class="px-4 py-3 w-72">
+              <span class="text-gray-400 mr-3 uppercase text-xs">Web App</span>
+              <p class="text-lg font-bold text-black truncate block capitalize">
+                OnyxCinema
+              </p>
+              <div class="flex items-center">
+                <div class="ml-auto">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+          <a href="#">
+            <img
+              src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Product"
+              class="h-80 w-72 object-cover rounded-t-xl"
+            />
+            <div class="px-4 py-3 w-72">
+              <span class="text-gray-400 mr-3 uppercase text-xs">Web App</span>
+              <p class="text-lg font-bold text-black truncate block capitalize">
+                OnyxCinema
+              </p>
+              <div class="flex items-center">
+                <div class="ml-auto">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+          <a href="#">
+            <img
+              src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="Product"
+              class="h-80 w-72 object-cover rounded-t-xl"
+            />
+            <div class="px-4 py-3 w-72">
+              <span class="text-gray-400 mr-3 uppercase text-xs">Web App</span>
+              <p class="text-lg font-bold text-black truncate block capitalize">
+                OnyxCinema
+              </p>
+              <div class="flex items-center">
+                <div class="ml-auto">
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </section>
+     </section>
+    </>
+  );
+}
