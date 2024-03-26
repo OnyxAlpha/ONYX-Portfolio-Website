2c3564929d111f1274b0fd153238f1c931585a4e
SADDA12 <sammiedaga@gmail.com>
2024-03-26 10:42:30 +0000

    working on skills

--- src/components/skills/index.jsx
+++ src/components/skills/index.jsx
@@ -10,37 +10,37 @@ export default function Skills() {
 
         <div className="grid grid-cols-3 p-10 md:grid-cols-5">
           <div className="bg-sky-200 text-center">
-            <p>icon</p>
+            <i class="fa-brands fa-html5"></i>
             <p>HTML5</p>
           </div>
 
           <div className="text-center">
-            <p>icon</p>
+            <i class="fa-brands fa-css3-alt"></i>
             <p>CSS</p>
           </div>
 
           <div className="bg-sky-200 text-center">
-            <p>icon</p>
+            <i class="fa-brands fa-js"></i>
             <p>JavaScript</p>
           </div>
 
           <div className="text-center">
-            <p>icon</p>
+            <i class="fa-brands fa-react"></i>
             <p>React JS</p>
           </div>
 
           <div className="bg-sky-200 text-center">
-            <p>icon</p>
+            <i class="fa-brands fa-node-js"></i>
             <p>Nodejs</p>
           </div>
 
           <div className="text-center">
-            <p>icon</p>
+          <i class="fa-brands fa-envira fa-rotate-by" style={{rotate: "40deg"}}></i>
             <p>MongoDB</p>
           </div>
 
           <div className="bg-sky-200 text-center">
-            <p>icon</p>
+            <i class="fa-brands fa-github"></i>
             <p>GitHub</p>
           </div>
 
@@ -75,22 +75,6 @@ export default function Skills() {
           </div>
         </div>
       </section>
-
-      <section id="skills" className="py-10">
-      <div className="container mx-auto">
-        <h2 className="text-3xl font-semibold mb-6">Skills</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Skill Card */}
-          <div className="bg-white p-6 rounded-lg shadow-md">
-            <h3 className="text-xl font-semibold mb-2">HTML</h3>
-            <p className="text-gray-700">Proficiency: Intermediate</p>
-            <p className="text-gray-700">Description: HTML is the standard markup language for creating web pages and web applications.</p>
-          </div>
-
-          {/* Add more skill cards here */}
-        </div>
-      </div>
-    </section>
     </>
   );
 }
