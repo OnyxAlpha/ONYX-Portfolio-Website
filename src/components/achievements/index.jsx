fc521d61fc04c07b577fc63a3df6d7e3a006327a
Hannan-Laaraba <ashraweelaaraba@gmail.com>
2024-03-27 14:09:42 +0000

    changes2

--- src/components/achievements/index.jsx
+++ src/components/achievements/index.jsx
@@ -1,62 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Achievements() {
-  
-    return (
-        <>
-         <section className="bg-gray-200">
-         <div class=" text-center p-10">
-            <h1 class="text-3xl">Achievements</h1>
-          </div>
-          <section
-            id="Projects"
-            class="w-fit pb-10 mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
-          >
-            <div class="w-96 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+  const [achievements, setAchievements] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from backend API
+    fetch(`${process.env.REACT_APP_ONYX_PORFOLIO_API}/achievements`)
+      .then((response) => response.json())
+      .then((data) => {
+        setAchievements(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []); 
+
+  return (
+    <>
+      <section className="bg-gray-200">
+        <div className="text-center p-10">
+          <h1 className="text-3xl">Achievements</h1>
+        </div>
+        <section
+          id="Projects"
+          className="w-fit pb-10 mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
+        >
+          {achievements.map((achievement) => (
+            <div
+              key={achievement.id}
+              className="w-96 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+            >
               <a href="#">
                 <img
-                  src="https://images.unsplash.com/photo-1610122748280-d0ae76b10750?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src={achievement.image}
                   alt="Product"
-                  class="h-80 w-96 object-cover rounded-t-xl"
+                  className="h-80 w-96 object-cover rounded-t-xl"
                 />
-                <div>
-                <i class="fa-solid fa-note-sticky p-4"> : </i> <br/>
-                <i class="fa-solid fa-calendar-days p-4"> : </i>  
+                <div className="p-4">
+                <i class="fa-solid fa-note-sticky p-4"> : </i>
+                  <p>{achievement.description}</p>
+                  <i class="fa-solid fa-calendar-days p-4"> : </i>  
+                  <p>{achievement.date}</p>
                 </div>
-                
               </a>
             </div>
-            <div class="w-96 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1610122748280-d0ae76b10750?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  alt="Product"
-                  class="h-80 w-96 object-cover rounded-t-xl"
-                />
-                <div>
-                <i class="fa-solid fa-note-sticky p-4"> : </i> <br/>
-                <i class="fa-solid fa-calendar-days p-4"> : </i>  
-                </div>
-                
-              </a>
-            </div>
-            <div class="w-96 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-              <a href="#">
-                <img
-                  src="https://images.unsplash.com/photo-1610122748280-d0ae76b10750?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  alt="Product"
-                  class="h-80 w-96 object-cover rounded-t-xl"
-                />
-                <div>
-                <i class="fa-solid fa-note-sticky p-4"> : </i> <br/>
-                <i class="fa-solid fa-calendar-days p-4"> : </i>  
-                </div>
-                
-              </a>
-            </div>
-          </section>
-         </section>
-         
-        </>
-      );
+          ))}
+        </section>
+      </section>
+    </>
+  );
 }
